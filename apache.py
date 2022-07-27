06c879e33b850d9d0c59743f79393bf7852b4a27
thommieboy0165 <106395185+thommieboy0165@users.noreply.github.com>
2022-07-27 10:21:00 +0000

    Add files via upload

--- apache.py
+++ apache.py
@@ -1,10 +1,15 @@
-import subprocess
+import time
 
-services = ['apache2']
-for service in services:
-    try:
-        status = subprocess.check_output("/etc/init.d/"+service+" status", shell=True)
-        print('Apache is running') 
-    except subprocess.CalledProcessError as e:
-        status = "is stopped"
-        print(status)
+def file():
+    while True:
+        with open('apache-status.txt') as f:
+            regels = f.readlines()
+            if 'active' in regels[2]:
+                print(".")
+            elif 'failed' in regels[2]:
+                tijd = (regels[2].split(' '))
+                print("[" + tijd[7] + ' ' + tijd[8] + '] Apache entered a failed state!' )
+            time.sleep(10)
+
+print("Starting to monitor apache-status.txt in current directory.")
+file()
